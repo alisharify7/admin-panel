d04dbaf9ad6a48f5a0448bd4e49db040320ff205
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2023-01-09 15:58:51 +0330

    UPDATE JS FILE

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,85 @@
+
+// set date in header
+let date_now = new Date()
+let dt = gregorian_to_jalali(date_now.getFullYear(), date_now.getMonth() + 1, date_now.getDate())
+let day = new String(dt[2]).length >= 2 ? dt[2] : "0" + new String(dt[2])
+let month = new String(dt[1]).length >= 2 ? dt[1] : "0" + new String(dt[1])
+document.querySelector(".persian-date-now").value =`${new String(dt[0])}-${new String(month)}-${new String(day)}`
+
+
+// set time in header
+window.setInterval(
+  (e)=>{
+    let time_now = new Date()
+    let time = `${time_now.getHours()}:${time_now.getSeconds()}`
+    document.querySelector(".persian-time-now").textContent=time
+  }, 1000
+)
+
+
+
+// side menu navbar
+
+
+let btn_close_menu = document.querySelector('.btn-close-menu')
+// side bar menu 
+btn_close_menu.addEventListener("click", (e)=>{
+  btn_close_menu.classList.toggle("closed")
+  if(btn_close_menu.children[0].className == "bi bi-arrow-right")
+  {
+    btn_close_menu.children[0].className = "bi bi-arrow-left"
+  }
+  else{
+    btn_close_menu.children[0].className = "bi bi-arrow-right"
+  }
+  document.querySelector(".menu-container").classList.toggle("close")
+  // document.querySelector(".menu-bottons-aside").classList.add("d-none")
+  document.querySelectorAll(".hidden-btn-menu").forEach(each=>{
+    each.classList.toggle("d-none")
+  })
+  document.querySelector(".navak-menu-icon").classList.toggle("d-none")
+  document.querySelector(".navak-asie-header-img").classList.toggle("d-none")
+  document.querySelector(".left-column").classList.toggle("open")
+  document.querySelector(".left-column").classList.toggle("col-md-8")
+  document.querySelector(".left-column").classList.toggle("col-lg-10")
+})
+
+
+
+// in mobile size aside nav menu closed automaticlly
+window.addEventListener("DOMContentLoaded", (e)=>{
+
+  window.addEventListener("resize", (e)=>{
+    if(window.innerWidth <= 1163){
+    if(!document.querySelector(".btn-close-menu").classList.contains("closed"))
+    {document.querySelector(".btn-close-menu").click()}
+  }
+  if(window.innerWidth >= 1163){
+    // if size is ok automaticlly open size navbar
+    if(document.querySelector(".btn-close-menu").classList.contains("closed"))
+    { document.querySelector(".btn-close-menu").click()  }
+  }
+  })
+
+if(window.innerWidth <= 1163){
+    if(!document.querySelector(".btn-close-menu").classList.contains("closed"))
+    {
+      document.querySelector(".btn-close-menu").click()
+    }
+}
+if(window.innerWidth >= 1163){
+// if size is ok automaticlly open size navbar
+    if(document.querySelector(".btn-close-menu").classList.contains("closed"))
+    {
+      document.querySelector(".btn-close-menu").click()
+    }
+}
+
+})
+
+
+
+// charts
 let done_projects = document.querySelector("#done-projects-container")
 let wait_projects = document.querySelector("#wait-projects-container")
 let doing_projects = document.querySelector("#doing-projects-container")
@@ -36,7 +118,7 @@ let chart_wait = new Chart(wait_projects, {
     options: {
       responsive: true,
       maintainAspectRatio: false,
-      scales: {
+        scales: {
         y: {
           beginAtZero: true
         }
@@ -66,59 +148,3 @@ let chart_doing = new Chart(doing_projects, {
   });
 
 
-
-let btn_close_menu = document.querySelector('.btn-close-menu')
-// side bar menu 
-btn_close_menu.addEventListener("click", (e)=>{
-  btn_close_menu.classList.toggle("closed")
-  if(btn_close_menu.children[0].className == "bi bi-arrow-right")
-  {
-    btn_close_menu.children[0].className = "bi bi-arrow-left"
-  }
-  else{
-    btn_close_menu.children[0].className = "bi bi-arrow-right"
-  }
-  document.querySelector(".menu-container").classList.toggle("close")
-  // document.querySelector(".menu-bottons-aside").classList.add("d-none")
-  document.querySelectorAll(".hidden-btn-menu").forEach(each=>{
-    each.classList.toggle("d-none")
-  })
-  document.querySelector(".navak-menu-icon").classList.toggle("d-none")
-  document.querySelector(".navak-asie-header-img").classList.toggle("d-none")
-  document.querySelector(".left-column").classList.toggle("open")
-  document.querySelector(".left-column").classList.toggle("col-md-8")
-  document.querySelector(".left-column").classList.toggle("col-lg-10")
-})
-
-
-
-// in mobile size aside nav menu closed automaticlly
-window.addEventListener("DOMContentLoaded", (e)=>{
-
-  window.addEventListener("resize", (e)=>{
-    if(window.innerWidth <= 768){
-    if(!document.querySelector(".btn-close-menu").classList.contains("closed"))
-    {document.querySelector(".btn-close-menu").click()}
-  }
-  if(window.innerWidth >= 948){
-    // if size is ok automaticlly open size navbar
-    if(document.querySelector(".btn-close-menu").classList.contains("closed"))
-    { document.querySelector(".btn-close-menu").click()  }
-  }
-  })
-
-if(window.innerWidth <= 768){
-        if(!document.querySelector(".btn-close-menu").classList.contains("closed"))
-        {
-          document.querySelector(".btn-close-menu").click()
-        }
-}
-if(window.innerWidth >= 948){
-// if size is ok automaticlly open size navbar
-        if(document.querySelector(".btn-close-menu").classList.contains("closed"))
-        {
-          document.querySelector(".btn-close-menu").click()
-        }
-}
-
-})
